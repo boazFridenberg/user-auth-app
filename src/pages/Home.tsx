e5e979239251167a505534c7d9adefc2e184fd47
boazFridenberg <boazfree152@gmail.com>
2025-09-14 11:35:48 +0300

    add reactBits

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,35 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import SplitText from "../components/SplitText";
 
 export default function Home() {
   return (
     <Box sx={{ textAlign: "center", mt: 6 }}>
-      <Typography variant="h3" gutterBottom>
-        Welcome to AuthApp
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 3 }}>
-        You can add to the app here!
-      </Typography>
-      <Button component={RouterLink} to="/register" variant="contained" sx={{ mr: 1 }}>
+      <SplitText
+        text="Welcome to AuthApp"
+        className="text-3xl font-bold"
+        delay={100}
+        duration={0.6}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        tag="h2"
+      />
+
+      <p style={{ marginTop: "10px", marginBottom: "30px" }}>
+        A simple user & admin demo with localStorage.
+      </p>
+
+      <Button
+        component={RouterLink}
+        to="/register"
+        variant="contained"
+        sx={{ mr: 1 }}
+      >
         Create account
       </Button>
       <Button component={RouterLink} to="/login" variant="outlined">
